8fc5a633509199b107fd4db62388c2a3a5f15f69
kavyaak19 <kavyaak19@gmail.com>
2026-05-29 15:56:19 +0530

    4th commit

---
+++ Day3/Day4/Add.java
@@ -0,0 +1,11 @@
+public class Add {
+    public static int addTwoNumbers(int a,int b){
+        int c = a+b;
+        return c;
+    }
+    public static void main(String[] args){
+       System.out.println(addTwoNumbers(11,22));
+      
+    }
+    
+}
---
+++ Day3/Day4/ArrayReverse.java
@@ -0,0 +1,18 @@
+public class ArrayReverse {
+    public static int[]reverse(int[]arr)
+        int n=arr.length;
+        for(int i=0;i<n/2;i++){
+            int temp=arr[i];
+            arr[i]=arr[n-1-i]
+            arr[n-1-i]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5};
+        reverse(a);
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]+" ");
+        }
+
+        
+    }
