9311a2774582f2f3a9d32b5a951bd149170d1237
kavyaak19 <kavyaak19@gmail.com>
2026-06-04 09:50:20 +0530

    updated java programs

---
+++ Day3/Day4/Day6/Day7/Day8/Day9/Abstarction.java
@@ -0,0 +1,20 @@
+abastract class vehicle{
+    abstract void start();
+    public void stop() {
+        System.out.println("stopped");
+    }
+}
+class Car extends vehicle{
+    public void start() {
+        System.out.println("car started");
+    }
+}
+public class Abstarction {
+    public static void main(String[] args) {
+        vehicle v = new Car();
+        v.start(); // Calls the implemented start method in Car class
+        v
+        .stop(); // Calls the inherited stop method from vehicle class
+    }
+    
+}
